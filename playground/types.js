b5f1be5e605cf83f39f6a633c9b21f9fa6af9322
RichKummer <rckummer@gmail.com>
2020-12-01 18:31:22 -0500

    07 through 11

---
+++ playground/types.js
@@ -0,0 +1,84 @@
+/* eslint-disable */
+/* const name = 'wes';
+const middle = "topher";
+const last = `bos`;
+
+const sentence = "she's so \"cool\"";
+const sentence2 = `she's so "cool"`;
+
+
+const song = `Ohh 
+
+ya 
+
+I like
+
+pizza`;
+
+
+const hello = "hello my name is " + name + ". Nice to meet you";
+
+const hello2 = `hello my name is ${name}. Nice to meet you`;
+
+const html = `
+    <div>
+        <h2>${name}</h2>
+        <p>${hello}</p>
+    </div>
+`;
+
+document.body.innerHTML = html;
+*/
+
+/*
+const age = 100;
+const name = `wes`;
+
+
+const a = 10;
+const b = 20;
+
+const smarties = 20;
+const kids = 3;
+const eachKidGets = Math.floor(smarties / kids);
+const dadGets = smarties % kids;
+console.log(`each kid gets ${eachKidGets}`);
+
+const price = 1034;
+
+*/
+
+/* 
+const person = {
+    first: 'wes',
+    last: 'bos',
+    age: 100
+};
+
+let dog;
+console.log(dog);
+
+let somethingUndefined;
+const somethingNull = null;
+
+
+const cher = {
+    first: 'cher',
+};
+
+const teller = {
+    first: 'Raymond',
+    last: 'Teller'
+}
+
+teller.first = 'Teller';
+teller.last = null;
+*/
+
+let isDrawing = false;
+
+let age = 18;
+const ofAge = age > 19;
+console.log(ofAge);
+
+age = 100;
