4f736fbc71d817b8eba53fe36375e2a69ee741ff
RichKummer <rckummer@gmail.com>
2020-12-19 13:05:57 -0500

    30 - Events

--- exercises/29 - Events/events.js
+++ exercises/29 - Events/events.js
@@ -16,6 +16,39 @@ butts.removeEventListener('click', handleClick);
 // Listen on multiple items
 const buyButtons = document.querySelectorAll('button.buy');
 
+function handleBuyButtonClick(event) {
+    console.log('you clicked a button!');
+    const button = event.target;
+    //console.log(button.textContent);
+    //console.log(parseFloat(event.target.dataset.price));
+    console.log(event.target);
+    console.log(event.currentTarget);
+    console.log(event.target === event.currentTarget);
+    //Stop this event from bubbling up/propogating
+    event.stopPropagation()
+}
+
+buyButtons.forEach(function(buyButton) {
+    buyButton.addEventListener('click', handleBuyButtonClick);
+});
+
+window.addEventListener('click', function() {
+    console.log(event.target);
+    console.log(event.BUBBLING_PHASE);
+    console.log(event.bubbles);
+    }, 
+    { capture: true}
+);
+
+const photoEl = document.querySelector(".photo");
+
+photoEl.addEventListener("mouseenter", function(e) {
+  console.log(e.currentTarget);
+  console.log(this);
+});
+
+
+/* Lesson 29
 function buyItem() {
     console.log('buying item!');
 }
@@ -31,3 +64,5 @@ buyButtons.forEach((button) => {
         console.log('You clicked it!');
     });
 });
+
+*/
