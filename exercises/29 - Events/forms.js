2a1242aa25884597c496279c88944fc3419342b9
RichKummer <rckummer@gmail.com>
2020-12-21 18:14:58 -0500

    31

---
+++ exercises/29 - Events/forms.js
@@ -0,0 +1,36 @@
+const rich = document.querySelector('.rich');
+
+rich.addEventListener('click', function(addEventListener) {
+
+    console.log('You clicked it');
+    const shouldChangePage = confirm('this website might be malicious! Do you want to proceed?');
+    if(!shouldChangePage) {
+        event.preventDefault();
+    }
+    console.log(shouldChangePage);
+});
+
+const signupForm = document.querySelector('[name="signup"]')
+
+signupForm.addEventListener('submit', function(event) {
+    const name = event.currentTarget.name.value;
+    if (name.includes('chad')) {
+        alert('Sorry bruh');
+        event.preventDefault();
+    }
+});
+
+function logEvent(event) {
+    console.log(event.type);
+    console.log(event.currentTarget.value);
+}
+
+signupForm.name.addEventListener('keyup', logEvent)
+signupForm.name.addEventListener('keydown', logEvent)
+signupForm.name.addEventListener('focus', logEvent)
+signupForm.name.addEventListener('blur', logEvent)
+
+// 'keyup'
+// 'keydown'
+// 'focus'
+// 'blur'
