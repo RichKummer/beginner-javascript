3ba6341fbb68eccca6dba69bfcdc3a483d2e0cf5
Rich Kummer <rckummer@gmail.com>
2024-03-07 15:09:52 -0500

    async-await-prompts-2

--- exercises/70 - Async prompts/scripts.js
+++ exercises/70 - Async prompts/scripts.js
@@ -33,6 +33,11 @@ function ask(options) {
             popup.firstElementChild.appendChild(skipButton);
 
             // listen for click on that cancel button
+            skipButton.addEventListener('click', function() {
+                resolve(null);
+                destroyPopup(popup);
+            }, {once: true}
+            );
         }
 
         // listen for submit event on the inputs
@@ -58,3 +63,57 @@ function ask(options) {
 
     });
 }
+
+// select all buttons that have a question
+async function askQuestion(e) {
+    const button = e.currentTarget;
+    const cancel = 'cancel' in button.dataset;
+
+    const answer = await ask({
+        title: button.dataset.question,
+        cancel,
+    });
+    console.log(answer);
+}
+
+const buttons = document.querySelectorAll('[data-question');
+buttons.forEach(button => button.addEventListener('click', askQuestion));
+
+console.log(buttons);
+
+const questions = [
+    { title: 'What is your name?' },
+    { title: 'What is your age?', cancel: true },
+    {title: 'What is your dogs name?'},
+];
+
+// utility function for asnyc mapping
+async function asyncMap(array, callback) {
+    // make an array to store results
+    const results = [];
+
+    // loop over our array
+    for(const item of array) {
+        results.push(await callback(item));
+    }
+
+    // when done with loop, return it
+    return results;
+
+}
+
+async function go() {
+    const answers = await asyncMap(questions, ask);
+    console.log(answers);
+}
+
+go();
+
+// async function askMany() {
+//     for (const question of questions) {
+//         const answer = await ask(question);
+//         console.log(answer);
+//     }
+// }
+
+// askMany();
